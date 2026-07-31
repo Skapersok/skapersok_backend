2d3b92caa712fdd7e8a54c8cd354a8a80d3b4715
Anders <57843932+2meanders@users.noreply.github.com>
2026-07-31 22:00:37 +0200

    Fixed misseg refactoring

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def main():
     if settings.autoopen_browser and not in_docker:
         threading.Thread(target=_open_browser, daemon=True).start()
 
-    uvicorn.run("main:app", host="0.0.0.0", port=settings.port)
+    uvicorn.run("routing:app", host="0.0.0.0", port=settings.port)
 
 
 if __name__ == "__main__":
